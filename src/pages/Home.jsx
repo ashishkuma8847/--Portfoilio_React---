8f8433b15dd10822997c39a276c33e01fd4bf4b8
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-08-18 13:04:57 +0530

    0000

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-    import { useTheme } from "../components/common/Themetoggel/ThemeProvider";
+import { useTheme } from "../components/common/Themetoggel/ThemeProvider";
 import Sideanimation from "../Home/Sideanimation";
 import Fontui from "../components/ui/Fontui";
 import Button from "../components/ui/Button";
 import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
+import aiGif from "../assets/Ai-powered.gif"
 const Home = () => {
     const { theme } = useTheme();
 
@@ -13,24 +14,29 @@ const Home = () => {
         <section className={`${theme ? "text-black" : "text-white"}`}>
             <div className="container flex justify-between items-center font-poppins">
                 <div className="flex flex-col justify-center gap-[30px]">
-                      
+
                     <h1 className="font-semibold text-[64px] w-[400px] leading-[65px]">Full-Stack<span className="text-customTeal"> Developer</span> </h1>
-                    <Fontui words={["Network & Telecom Student","Tech Enthusiast"]} outofspan=""/>
+                    <Fontui words={["Network & Telecom Student", "Tech Enthusiast"]} outofspan="" />
                     <div className="flex gap-[15px]">
-                        
-                    <Button title={"React"}/>
-                    <Button title={"Javascript"}/>
-                    <Button title={"Node.js"}/>
-                    <Button title={"Tailwind"}/>
+
+                        <Button title={"React"} />
+                        <Button title={"Javascript"} />
+                        <Button title={"Node.js"} />
+                        <Button title={"Tailwind"} />
                     </div>
                     <div className="flex gap-[15px]">
-                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" :"bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandGithub /> </button>
-                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" :"bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandInstagram  /> </button>
-                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" :"bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandLinkedin   /> </button>
+                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" : "bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandGithub /> </button>
+                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" : "bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandInstagram /> </button>
+                        <button className={`${theme ? "bg-white/50 border-black/10 group-hover:border-black/20" : "bg-black/50 border-white/10 group-hover:border-white/20"} relative rounded-xl  backdrop-blur-xl p-2 flex items-center justify-center border  transition-all  duration-300`}><IconBrandLinkedin /> </button>
                     </div>
 
                 </div>
-                <Sideanimation />
+                {/* <Sideanimation /> */}
+                <img
+                    src={aiGif}
+                    alt="AI Powered Animation"
+                    className="w-80 h-80 object-contain drop-shadow-lg"
+                />
             </div>
         </section>
 
