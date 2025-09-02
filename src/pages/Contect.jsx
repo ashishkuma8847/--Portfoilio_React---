814dd66c75671dbb4b43752bd26f31f254037f01
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-09-02 12:31:42 +0530

    dfsd

--- src/pages/Contect.jsx
+++ src/pages/Contect.jsx
@@ -1,14 +1,21 @@
 import { useTheme } from "../components/Themetoggel/ThemeProvider";
-import  { useState } from "react";
+import { useState } from "react";
 import { Mail, Phone, MapPin, Send } from "lucide-react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function Contect() {
     const { theme } = useTheme();
-  const [loading, setLoading] = useState(false);
-   const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setTimeout(() => setLoading(false), 2000);
-  };
+    const [loading, setLoading] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setTimeout(() => setLoading(false), 2000);
+    };
+    AOS.init({
+        duration: 1200,
+        once: false,
+        offset: 0,
+    });
     return (
         <>
             <section className={`${theme ? "text-black" : "text-white"} `}>
@@ -18,35 +25,35 @@ function Contect() {
                 >
                     <div className="container justify-center px-4  flex items-center   h-screen gap-10 py-20">
                         {/* Left Side */}
-                        <div className="flex-1 flex flex-col gap-6 max-w-[400px] w-full ">
-                            <h1 className="text-4xl font-semibold text-customTeal flex gap-[10px]">
+                        <div data-aos="fade-right"  data-aos-delay="200" className="flex-1 flex flex-col gap-6 max-w-[400px] w-full ">
+                            <h1 data-aos="fade-right" className="text-4xl font-semibold text-customTeal flex gap-[10px]">
                                 Let’s Connect <iframe title="id" src="https://lottie.host/embed/a8c326ab-79af-4c6a-ae2e-44fce48336e3/CguXzoecsQ.lottie " className="w-[34px] h-[34px]"></iframe>
                             </h1>
-                            <h2 className="text-2xl font-bold">
+                            <h2 data-aos="fade-right"  data-aos-delay="400" className="text-2xl font-bold">
                                 Let’s <span className="text-customTeal">Work Together!</span>
                             </h2>
-                            <p className="text-lg leading-relaxed">
+                            <p data-aos="fade-right"  data-aos-delay="600" className="text-lg leading-relaxed">
                                 I’m always open to discussing new projects, creative ideas, or
                                 opportunities to be part of your vision.
                             </p>
 
                             {/* Contact Info */}
-                            <div className="flex flex-col gap-3">
-                                <p className="flex items-center gap-2  z-0">
+                            <div  className="flex flex-col gap-3">
+                                <p data-aos="fade-right"  data-aos-delay="800" className="flex items-center gap-2  z-0">
                                     <Mail className="text-customTeal" />
                                     <a href="mailto:ashishkuma8847@gmail.com" className=" hover:underline hover:text-customTeal">
                                         ashishkuma8847@gmail.com
                                     </a>
                                 </p>
 
-                                <p className="flex items-center gap-2 z-0">
+                                <p data-aos="fade-right"  data-aos-delay="1000" className="flex items-center gap-2 z-0">
                                     <Phone className="text-customTeal" />
                                     <a href="tel:+916284978409" className="hover:underline hover:text-customTeal">
                                         +91 6284978409
                                     </a>
                                 </p>
 
-                                <p className="flex items-center gap-2 z-0">
+                                <p data-aos="fade-right"  data-aos-delay="1200"  className="flex items-center gap-2 z-0">
                                     <MapPin className="text-customTeal" />
                                     <a href="https://www.google.com/maps/place/Abohar,+Punjab,+India" target="_blank" className="hover:underline hover:text-customTeal">Abohar, Punjab, India</a>
                                 </p>
@@ -59,22 +66,22 @@ function Contect() {
                                 onSubmit={handleSubmit}
                                 className="flex flex-col gap-4 mt-6 bg-transparent "
                             >
-                                <input
+                                <input data-aos="fade-down"  data-aos-delay="800"
                                     type="text"
                                     placeholder="Your Name"
                                     className="bg-transparent border z-0 border-gray-600 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-customTeal"
                                 />
-                                <input
+                                <input data-aos="fade-down" data-aos-delay="600"
                                     type="email"
                                     placeholder="Your Email"
                                     className="bg-transparent border z-0 border-gray-600 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-customTeal"
                                 />
-                                <textarea
+                                <textarea data-aos="fade-down" data-aos-delay="400"
                                     placeholder="Your Message"
                                     rows="5"
                                     className="bg-transparent border z-0 border-gray-600 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-customTeal"
                                 ></textarea>
-                                <button
+                                <button data-aos="fade-down" data-aos-delay="200"
                                     type="submit"
                                     disabled={loading}
                                     className="bg-customTeal z-0 text-white font-semibold py-3 px-6 rounded-xl hover:scale-105 transition-transform duration-200 shadow-lg disabled:opacity-50 mb-16 md:mb-0"
